8b485c888304351fff01d54f953f0e42509935e2
Matthew Perez <matthew203465083@gmail.com>
2014-08-13 14:52:34 -0400

    This contains the login/signup screens parse stuff it will later contain the file upload system

---
+++ JavaSript/signup-screen.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+  Parse.initialize("NzjDI96DNyAS8U4DNWYgGxCS8lSg9kyks2azZ2UC","Mvi4oDnLqRqImITOqn1pN1BwdejN6PvLoUnpVL6I");
+
+  $("#new-user-submit").click(function()) {
+
+        // 2. Create a new Parse.User.
+        var user = new Parse.User();
+
+        // 3. Set a username.
+        user.set('username', $('#new-username').val());
+
+        // 4. Set a password.
+        user.set('password', $('#new-password').val());
+        
+        user.set('email', $('#new-email').val());
+        // 5. Sign them up!
+        user.signUp(null, {
+          success: function(user) {
+            alert("User signed up!");
+          },
+          error: function(error) {
+            alert("Error: "+error.code+" "+error.message)
+          }
+        });
+      });
+    $('#existing-user-submit').click(function()) {
+
+      Parse.User.logIn( $('#existing-username').val $('#existing-password').val  , {
+        success: function(user) {
+        // Do stuff after successful login.
+      },
+      error: function(user, error) {
+        // The login failed. Check error to see why.
+      }
+    });
+    $('#upload-file-button').click(function()) {
+
+    }
+    
+});
+
