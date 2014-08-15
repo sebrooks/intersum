0bb5bcc013ec082931bb3a9c356ed0ff873b16f0
matthewP96 <matthew203465083@gmail.com>
2014-08-15 16:07:45 -0400

    Update discussion.js

--- JavaSript/discussion.js
+++ JavaSript/discussion.js
@@ -1,12 +1,17 @@
-$(document).ready(function(
-	Parse.initialize("NzjDI96DNyAS8U4DNWYgGxCS8lSg9kyks2azZ2UC","F5p230fkKINZ5KP44qR7hk7isWdD9JXa2Tk1rrjm");
+$(document).ready(function(){
+  Parse.initialize("NzjDI96DNyAS8U4DNWYgGxCS8lSg9kyks2azZ2UC","F5p230fkKINZ5KP44qR7hk7isWdD9JXa2Tk1rrjm");
 
 	$("#begin-new-post-button").click(function() {
+
+
+if (post) {};
+		var postText =  $("#begin-new-post-text").val();
 		var Post = Parse.Object.extend("Post");
 		var myPost = new Post();
 
-		myPost.set( $("#begin-new-post-text").val );
+		myPost.set(postText);
 
 		myPost.save()
 	});
-));
+});
+
