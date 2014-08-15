3024fde0be7300122e705a652e5fa43146fee9a3
hollynahar <hollynahar@gmail.com>
2014-08-15 16:10:36 -0400

    Update fileupload.js

--- JavaSript/fileupload.js
+++ JavaSript/fileupload.js
@@ -2,22 +2,30 @@ $(document).ready(function(){
 	Parse.initialize("NzjDI96DNyAS8U4DNWYgGxCS8lSg9kyks2azZ2UC","F5p230fkKINZ5KP44qR7hk7isWdD9JXa2Tk1rrjm");
 
 	$("#upload-file-button").click(function() {
+		var base64 = "V29ya2luZyBhdCBQYXJzZSBpcyBncmVhdCE=";
 
-		var file = new Parse.File("myfile.zzz", fileData, "image/png"); {
+		var upload = $("#new-fileupload").val();
+		var file= new Parse.File(upload".txt", { base64: base64 });
+		var file = new Parse.File(upload".zzz","image/png");
+		
 
-			var fileUpLoadControl = $("#new-fileupload")[0];
-			if(fileUpLoadControl.files.length > 0) {
-				var file = fileUpLoadControl.file[0];
-				var name = $("#new-file-name").val;
+		var fileUploadControl = $("#upload-file-button")[0];
+		if (fileUploadControl.files.length > 0) {
+			var file = fileUploadControl.files[0];
+			var name = "photo.jpg";
 
-				var parseFile = new Parse.File(name, file);
-			}
-
-			parseFile.save().then(function()  {
-				console.log("File save successful!")
-			}, function(error)
-			console.log("Error:" + error.code + " " + error.message);
+			var parseFile = new Parse.File(name, file);
 		}
-	});
-});
+		parseFile.save().then(function()  {
+			console.log("File save successful!")
+		}, 
+		function(error) {
+		console.log("Error:" + error.code + " " + error.message);
+	   }
+
+
+   });
+}); 
+
+
 
