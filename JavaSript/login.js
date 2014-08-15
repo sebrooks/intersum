2c76ba18edca197af78e2dbec703a73684b8d6aa
matthewP96 <matthew203465083@gmail.com>
2014-08-15 01:37:59 -0400

    Create login.js

---
+++ JavaSript/login.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	Parse.initialize("NzjDI96DNyAS8U4DNWYgGxCS8lSg9kyks2azZ2UC","F5p230fkKINZ5KP44qR7hk7isWdD9JXa2Tk1rrjm");
+
+
+	$("#existing-user-submit").click(function() {
+
+		var username = $("#existing-username").val();
+		var password = $("#existing-password").val();
+
+		Parse.User.logIn (username, password){
+
+			success: function(user) {
+				console.log("User Logged in!");
+			},
+			error: function(user, error) {                 
+				console.log("Error:" + error.code + " " + error.message);
+			}
+		});
+
+});
+
