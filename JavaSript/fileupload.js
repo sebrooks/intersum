2b2fd06022dc7a9cb7b0e686a41cd54899b30747
matthewP96 <matthew203465083@gmail.com>
2014-08-15 17:53:45 -0400

    Update fileupload.js

--- JavaSript/fileupload.js
+++ JavaSript/fileupload.js
@@ -1,18 +1,17 @@
 $(document).ready(function(){
 	Parse.initialize("NzjDI96DNyAS8U4DNWYgGxCS8lSg9kyks2azZ2UC","F5p230fkKINZ5KP44qR7hk7isWdD9JXa2Tk1rrjm");
 
-	$("#upload-file-button").click(function() {
+	$("#upload-file-button").click(function(){
 		var base64 = "V29ya2luZyBhdCBQYXJzZSBpcyBncmVhdCE=";
-
 		var upload = $("#new-fileupload").val();
-		var file= new Parse.File(upload".txt", { base64: base64 });
-		var file = new Parse.File(upload".zzz","image/png");
+		var file= new Parse.File(upload + ".txt", { base64: base64 });
+		var file = new Parse.File(upload + ".zzz","image/png");
 		
 
 		var fileUploadControl = $("#upload-file-button")[0];
 		if (fileUploadControl.files.length > 0) {
 			var file = fileUploadControl.files[0];
-			var name = "photo.jpg";
+			var name = upload + ".png";
 
 			var parseFile = new Parse.File(name, file);
 		}
@@ -20,12 +19,9 @@ $(document).ready(function(){
 			console.log("File save successful!")
 		}, 
 		function(error) {
-		console.log("Error:" + error.code + " " + error.message);
-	   }
+			console.log("Error:" + error.code + " " + error.message);
+		});
+	
 
-
-   });
-}); 
-
-
-
+	}); 
+});
