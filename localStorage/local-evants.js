671c87142d71790b19710faa299bf87a1b66a8b3
Matthew Perez <matthew203465083@gmail.com>
2014-08-26 19:19:56 -0400

    adding new events

--- localStorage/local-evants.js
+++ localStorage/local-evants.js
@@ -1,23 +1,24 @@
 $(document).ready(function(){
 
-var eventItem = $("<li>")
-var eventContents = $("<a>")
+	var eventItem = $("<li>")
+	var eventContents = $("<a>")
 	
 
 	$("#create-event-submit").click(function(){
 
-	localStorage.newEventName = $("#create-event-name").val();
-	localStorage.newEventLocation = $("#create-event-location").val();
-	localStorage.newEventStartTime = $("#create-event-start-time").val();
-	localStorage.newEventEndTime = $("#create-event-end-time").val();
-	localStorage.newEventDetails = $("#create-event-details").val();
+		localStorage.newEventName = $("#create-event-name").val();
+		localStorage.newEventLocation = $("#create-event-location").val();
+		localStorage.newEventStartTime = $("#create-event-start-time").val();
+		localStorage.newEventEndTime = $("#create-event-end-time").val();
+		localStorage.newEventDetails = $("#create-event-details").val();
 
-	eventItem.addClass("table-view-cell");
-	eventContents.addClass("navigate-right");
-	eventItem.html(eventContents);
-	eventContents.html(localStorage.newEventName);
-	$("li.table-view-cell:last").after(eventItem);
-	
+		eventItem.addClass("table-view-cell");
+		eventContents.addClass("navigate-right");
+		eventItem.html(eventContents);
+		eventContents.html(localStorage.newEventName);
+		$("li.table-view-cell:last").after(eventItem);
+		$("#openModal").hide()
 	});
+	
 
 });
