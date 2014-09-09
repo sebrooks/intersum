1ea932d86c44efb0d0822601e4cb527bacc251f5
Matthew Perez <matthew203465083@gmail.com>
2014-09-09 01:54:22 -0400

    I changed the index back to the log in page since we don't have the time to do a splash page
    i have edited the JavaScript file so that it should allow the links  to be loaded based on the discussion saved in parse, it should also allow you to save more discussions to parse
    the second part has been confirmed to work because i have tested it
    unfortunately i cant get the discussions to load because there are syntax errors which i will contact Jason and Ilya for
    otherwise we may have a semi functioning app for the semi finals

--- JavaScript/discussion.js
+++ JavaScript/discussion.js
@@ -1,20 +1,37 @@
 $(document).ready(function(){
 	Parse.initialize("NzjDI96DNyAS8U4DNWYgGxCS8lSg9kyks2azZ2UC","F5p230fkKINZ5KP44qR7hk7isWdD9JXa2Tk1rrjm");
 
-	$("#post-new-disscusion-button").click(function() {
+	var query = new Parse.Query("Discussions")
+	query.equalTo('groupID', 'TtQYY23CHY')
+	query.find({
+		success: function(objects) {
+			for (i=0; i<objects.length; i++) { 
 
-		var Topic =  $("#begin-new-disscusion").val();
-		var Discussions = Parse.Object.extend("Discussions");
+				var discussionTopic = objects[i].get("Topic");
+				var discussionItem = $("<li>")
+				var discussionContents = $("<a>")
 
-		if (Topic) {
-			var myDiscussions = new Discussions();
-
-			myDiscussions.set("Topic", Topic);
-			myDiscussions.save();
-		};
+				discussionItem.addClass("table-view-cell");
+				discussionContents.addClass("navigate-right");
+				discussionItem.html(discussionContents);
+				discussionContents.html(discussionTopic);
+				$("li.table-view-cell:first").before(discussionItem);
+				$("div#openModal").fadeOut("slow");
+			}
+		},
+		error: function(error) {
+			console.log("Error: " + error.code + " " + error.message);
+		}
 	});
 
-
+	var Discussions = Parse.Object.extend("Discussions");
+	var discussions = new Discussions();
+	$("#new-disscusion-button").click(function(){
+		var subject = $("#new-disscusion-topic").val();
+		var text = $("#new-disscusion-text").val();
+		discussions.set("Topic", subject);
+		discussions.set("Description", text);
+		discussions.set("groupID", "TtQYY23CHY");
+		discussions.save();
+	});
 });
-
-
--- JavaScript/splashPage.js
+++ JavaScript/splashPage.js
@@ -1,5 +1,39 @@
 $(document).ready(function(){
-	$("#splash").click(function(){
-		$("Index.html").fadeOut(3000);
+	Parse.initialize("NzjDI96DNyAS8U4DNWYgGxCS8lSg9kyks2azZ2UC","F5p230fkKINZ5KP44qR7hk7isWdD9JXa2Tk1rrjm");
+
+	$('div#login-page').hide();
+	$('h2#splash-name').hide().delay(800).fadeIn("slow");
+
+	$("body").mousedown(function(){
+
+		$("img").delay(250).slideup(100);
+		$("h1").fadeOut(250);
+		$('div#login-page').delay(250).slideup(100);
 	});
+
+	$("#existing-user-login").click(function() {
+
+		var username = $("#existing-username").val();
+		var password = $("#existing-password").val();
+
+		if (username && password) {
+
+			localStorage.username = $("#existing-username").val();
+			Parse.User.logIn( username, password)
+
+			location.assign("clubs.html");	
+		}
+
+	});
+
 });
+
+
+
+
+
+
+/*
+		location.assign("login.html");
+
+  */
