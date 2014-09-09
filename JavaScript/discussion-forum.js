e41d7a97cbf35a0bb7af9bc1ea03a20722c03cd2
Matthew Perez <matthew203465083@gmail.com>
2014-09-09 02:31:36 -0400

    working on the posts i have to set the discussion id to the current discussion for the query to work and i have to load them to the page

--- JavaScript/discussion-forum.js
+++ JavaScript/discussion-forum.js
@@ -1,36 +1,55 @@
 $(document).ready(function(){
 	Parse.initialize("NzjDI96DNyAS8U4DNWYgGxCS8lSg9kyks2azZ2UC","F5p230fkKINZ5KP44qR7hk7isWdD9JXa2Tk1rrjm");
-
-
-
-	var Discussions = Parse.Object.extend("Discussions");
-	var Post = Parse.Object.extend("Post");
-	var text =  $("#begin-new-post-text").val();
-
-	$("#begin-new-post-button").click(function() {
-
-		if (text) {
-			var myPost = new Post();
-
-			myPost.set("text", text);
-			myPost.save();
-		};
-	});
-
-
-	var innerQuery = new Parse.Query(Discussions);
-	var query = new Parse.Query(Post);
-
-	innerQuery.equalTo("objectid","9gxhUv40wg");
-	query.matchesQuery("discussionID", innerQuery);
-
+	$("li.table-view-cell:first").hide();
+/*
+	var query = new Parse.Query("Post")
+	query.equalTo('', INSERT VARIABLE)
 	query.find({
-		success: function(Discussions) {
-			alert("IT WORKS")
+		success: function(objects) {
+			for (i=0; i<objects.length; i++) { 
+
+				var postTopic = objects[i].get("text");
+				var postItem = $("<li>")
+				var postnContents = $("<a>")
+
+				postItem.addClass("table-view-cell");
+				postContents.addClass("navigate-right");
+				postItem.html(postContents);
+				postContents.html(postTopic);
+				$("li.table-view-cell:last").after(postItem);
+			}
 		},
 		error: function(error) {
-			alert("Error: " + error.code + " " + error.message)
+			console.log("Error: " + error.code + " " + error.message);
 		}
 	});
+*/
+	$("#post-submit").click(function(){
+
+	var Post = Parse.Object.extend("Post");
+	var post = new Post();
+	var postText = $("#post-responce").val();
+
+
+		post.set("text", postText);
+//		post.set("disscusionID", INSERT VARIABLE )
+//		post.set("user", INSERT VARIABLE)
+		post.save();
+
+	});
+
 
 });
+
+/*
+
+var discussionItem = $("<li>")
+var discussionContents = $("<a>")
+
+discussionItem.addClass("table-view-cell");
+discussionContents.addClass("navigate-right");
+discussionItem.html(discussionContents);
+discussionContents.html(localStorage.discussionTitle);
+$("li.table-view-cell:first").before(discussionItem);
+
+*/
