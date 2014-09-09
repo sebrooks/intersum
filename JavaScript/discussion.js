88b7e826c2784b6830fced08b3c6680710599a24
Matthew Perez <matthew203465083@gmail.com>
2014-09-09 18:40:17 -0400

    Pushing Ilya's changes

--- JavaScript/discussion.js
+++ JavaScript/discussion.js
@@ -1,40 +1,43 @@
 $(document).ready(function(){
 	Parse.initialize("NzjDI96DNyAS8U4DNWYgGxCS8lSg9kyks2azZ2UC","F5p230fkKINZ5KP44qR7hk7isWdD9JXa2Tk1rrjm");
-
-	$("li.table-view-cell:first").hide();
-
-	var query = new Parse.Query("Discussions")
-	query.equalTo('groupID', 'TtQYY23CHY')
-	query.find({
-		success: function(objects) {
-			for (i=0; i<objects.length; i++) { 
-
-				var discussionTopic = objects[i].get("Topic");
-				var discussionItem = $("<li>")
-				var discussionContents = $("<a>")
-
-				discussionItem.addClass("table-view-cell");
-				discussionContents.addClass("navigate-right");
-				discussionContents.attr("href","disscussion-template.html")
-				discussionItem.html(discussionContents);
-				discussionContents.html(discussionTopic);
-				$("li.table-view-cell:first").before(discussionItem);
-				$("div#openModal").fadeOut("slow");
-			}
-		},
-		error: function(error) {
-			console.log("Error: " + error.code + " " + error.message);
-		}
+        loadContentFromParse();
+ 
+ 	var Discussions = Parse.Object.extend("Discussions");
+ 	var discussions = new Discussions();
+ 	$("#new-disscusion-button").click(function(){
+ 		var subject = $("#new-disscusion-topic").val();
+ 		var text = $("#new-disscusion-text").val();
+ 		discussions.set("Topic", subject);
+ 		discussions.set("Description", text);
+ 		discussions.set("groupID", "TtQYY23CHY");
+ 		discussions.save();
+                $("div#openModal").fadeOut("slow");
+                loadContentFromParse();
 	});
 
-	var Discussions = Parse.Object.extend("Discussions");
-	var discussions = new Discussions();
-	$("#new-disscusion-button").click(function(){
-		var subject = $("#new-disscusion-topic").val();
-		var text = $("#new-disscusion-text").val();
-		discussions.set("Topic", subject);
-		discussions.set("Description", text);
-		discussions.set("groupID", "TtQYY23CHY");
-		discussions.save();
+	function loadContentFromParse(){
+          var query = new Parse.Query("Discussions")
+          query.equalTo('groupID', 'TtQYY23CHY')
+          query.find({
+                  success: function(objects) {
+                          for (i=0; i<objects.length; i++) {
+
+                                  var discussionTopic = objects[i].get("Topic");
+                                  var discussionItem = $("<li>")
+                                  var discussionContents = $("<a>")
+
+                                  discussionItem.addClass("table-view-cell");
+                                  discussionContents.addClass("navigate-right");
+                                  discussionContents.attr("href","disscussion-template.html")
+                                  discussionItem.html(discussionContents);
+                                  discussionContents.html(discussionTopic);
+                                  $("li.table-view-cell:first").before(discussionItem);
+                          }
+                  },
+                  error: function(error) {
+                          console.log("Error: " + error.code + " " + error.message);
+                  }
 	});
-});
+
+	}
+ });
