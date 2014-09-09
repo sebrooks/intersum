ece1db38d66552997d2e96ef5487a32c797fca61
Matthew Perez <matthew203465083@gmail.com>
2014-09-09 18:13:42 -0400

    stuff

--- JavaScript/discussion.js
+++ JavaScript/discussion.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 	Parse.initialize("NzjDI96DNyAS8U4DNWYgGxCS8lSg9kyks2azZ2UC","F5p230fkKINZ5KP44qR7hk7isWdD9JXa2Tk1rrjm");
 
+	$("li.table-view-cell:first").hide();
+
 	var query = new Parse.Query("Discussions")
 	query.equalTo('groupID', 'TtQYY23CHY')
 	query.find({
